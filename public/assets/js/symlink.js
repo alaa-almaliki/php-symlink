4c8382420f2de88fcb4677833a3a5ec33d18c257
alaa-almaliki <alaa.almaliki@gmail.com>
2017-02-19 08:23:08 +0800

    Add fix to incorrect statement

--- public/assets/js/symlink.js
+++ public/assets/js/symlink.js
@@ -104,7 +104,7 @@ Symlink.Ajax.prototype.send = function (options, callback, async) {
 
     this.xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            if (callback === 'undefined') {
+            if (typeof callback === 'undefined') {
                 return this;
             }
             return callback(this);
